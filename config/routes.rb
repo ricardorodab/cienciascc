ba0fb098b7a9f03e7bc46415a330803e84445873
ricardorodab <ricardorodab19@gmail.com>
2016-07-29 02:43:07 -0500

    rutas a las materias actuales

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'materias/lenguajes'
+  get 'materias/lenguajes', :as => :lenguajes
 
-  get 'materias/discretas'
+  get 'materias/discretas', :as => :discretas
 
-  get 'materias/icc'
+  get 'materias/icc', :as => :icc
 
   get 'welcome/index'
 
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   root 'welcome#index'
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
